ad7d8d91eac3e4d64123554fbfedf9f24be7f887
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-07 20:50:53 -0600

    made this commit to as added env file with link

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-    .connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
