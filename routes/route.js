b8c0639ef6a76010482d4eceaa1cb786b838c463
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-07 20:23:52 -0600

    added restful api operations in route

--- routes/route.js
+++ routes/route.js
@@ -2,3 +2,12 @@ const { Router } = require("express");
 const { getTravel, saveTravel, deleteTravel, updateTravel } = require("../controllers/TravelController");
 const router = Router();
 
+router.get("/", getTravel);
+
+router.post("/add", saveTravel);
+
+router.put("/update", updateTravel);
+
+router.post("/delete", deleteTravel); //changed delete to post , because axios is not making delete work !!!
+
+module.exports = router;
