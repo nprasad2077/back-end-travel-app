8234482eb81c56b054fac61515f321667df18205
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-07 20:24:33 -0600

    changed index.js , variable routes, to route.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors")
-const routes = require("./routes/TravelRoute");
+const routes = require("./routes/route");
 const app = express();
 const PORT = process.env.PORT | 5000;
 
