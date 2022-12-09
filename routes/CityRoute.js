e18e0ac8bf5680cfea520a1492f0c836f25a7b36
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-09 14:13:52 -0600

    delete method added

--- routes/CityRoute.js
+++ routes/CityRoute.js
@@ -10,6 +10,6 @@ router.post("/add", saveCity);
 
 router.put("/update", updateCity);
 
-router.post("/delete", deleteCity); //changed delete to post , because axios is not making delete work !!!
+router.delete("/delete", deleteCity); //changed delete to post , because axios is not making delete work !!!
 
 module.exports = router;
